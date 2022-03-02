f423333c24dc31435a831a50a940de0c1346ceb2
tammer81 <tammer.riad@gmail.com>
2022-03-02 11:24:16 -0800

    Added text tokenizer

--- app/cleantext.py
+++ app/cleantext.py
@@ -1,16 +1,26 @@
 import re
+import spacy
+nlp = spacy.load("en_core_web_md")
+
 
 def clean_doc(text):
-  punct_regex = r"[^0-9a-zA-Z\s]" # any non-alphanumeric chars
-  special_chars_regex = r"[\$\%\&\@+]" 
-  whitespace_regex = r"\s+"
-  newline_regex = r"\n+"
+    punct_regex = r"[^0-9a-zA-Z\s]"
+    special_chars_regex = r"[\$\%\&\@+]"
+    whitespace_regex = r"\s+"
+    newline_regex = r"\n+"
 
-  text = re.sub(punct_regex, "", text)
-  text = re.sub(special_chars_regex, "", text)
-  text = re.sub(whitespace_regex, " ", text)
-  text = re.sub(newline_regex, " ", text)
+    text = re.sub(punct_regex, "", text)
+    text = re.sub(special_chars_regex, "", text)
+    text = re.sub(whitespace_regex, " ", text)
+    text = re.sub(newline_regex, " ", text)
 
-  # case normalize and strip extra white spaces on the far left and right hand side
-  text = text.lower().strip()
-  return text
+    text = text.lower().strip()
+    return text
+
+
+def tokenizer(text):
+    tokens = []
+    for token in nlp(text):
+        if (not token.is_punct) & (not token.is_space):
+            tokens.append(token.text.lower())
+    return tokens
